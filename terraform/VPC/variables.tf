0be335784f568cbb9675ddcbfc062f60c3b44d63
sylbardau <bardau.sylvain@gmail.com>
2026-05-17 14:45:42 +0000

    Changes to be committed: 	modified:   VPC/variables.tf ajout de la variable en double

--- terraform/VPC/variables.tf
+++ terraform/VPC/variables.tf
@@ -1,5 +1,12 @@
 # Variables pour les fichiers terraform
 
+variable "aws_region" {
+  description = "Région AWS pour le déploiement"
+  type        = string
+  default     = "eu-west-3" # Paris
+}
+
+
 variable "vpc_cidr" {
   description = "Plage IP CIDR pour le VPC"
   type        = string
