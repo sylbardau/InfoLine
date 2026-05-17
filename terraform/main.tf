ae36d7ff2e27f2f38709a765c9fa5a5a7cfa4578
sylbardau <bardau.sylvain@gmail.com>
2026-05-17 15:40:24 +0000

    Changes to be committed: 	modified:   main.tf déplacemnt du corchet pour prise en compte des argument dans le module

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,10 @@
 
 module "vpc" {
   source = "./VPC"
-}
+
   # ajout des variables VPC
   aws_region       = var.aws_region
   vpc_cidr         = var.vpc_cidr
   cidr_subnet_dev  = var.cidr_subnet_dev
   cidr_subnet_prod = var.cidr_subnet_prod
+}
