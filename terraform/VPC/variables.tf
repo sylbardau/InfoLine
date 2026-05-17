2fbcf00ce9bbc3eb23c76d584f2bfe2e7c756b05
sylbardau <bardau.sylvain@gmail.com>
2026-05-17 15:35:46 +0000

    Changes to be committed: 	modified:   VPC/variables.tf 	modified:   main.tf 	modified:   variable.tf
    Modification des variable pour la prise en compte des variable du dossier principal dans le dossier VPC

--- terraform/VPC/variables.tf
+++ terraform/VPC/variables.tf
@@ -1,26 +1,21 @@
-# Variables pour les fichiers terraform
+# Variables pour le fichiers VPC/main.tf
 
 variable "aws_region" {
-  description = "Région AWS pour le déploiement"
   type        = string
-  default     = "eu-west-3" # Paris
+  description = "Région AWS"
 }
 
-
 variable "vpc_cidr" {
-  description = "Plage IP CIDR pour le VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  description = "Bloc CIDR du VPC"
 }
 
 variable "cidr_subnet_dev" {
-  description = "CIDR pour le sous-réseau de Dev"
   type        = string
-  default     = "10.0.2.0/24"
+  description = "Bloc CIDR de sous-réseau dev"
 }
 
 variable "cidr_subnet_prod" {
-  description = "CIDR pour le sous-réseau de Prod"
   type        = string
-  default     = "10.0.1.0/24"
+  description = "Bloc CIDR de sous-réseau prod"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,8 @@
 module "vpc" {
   source = "./VPC"
 }
+  # ajout des variables VPC
+  aws_region       = var.aws_region
+  vpc_cidr         = var.vpc_cidr
+  cidr_subnet_dev  = var.cidr_subnet_dev
+  cidr_subnet_prod = var.cidr_subnet_prod
