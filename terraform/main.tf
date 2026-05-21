ad421b73bee3e17c799dbd302346054936695fc0
sylbardau <bardau.sylvain@gmail.com>
2026-05-21 10:11:48 +0000

    Changes to be committed: 	modified:   main.tf Modification des variables attendu pour l'execution du terraform plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,8 @@ module "vpc" {
   # ajout des variables VPC
   aws_region       = var.aws_region
   vpc_cidr         = var.vpc_cidr
-  cidr_subnet_loadbalancer_a  = var.cidr_loadbalancer_a
-  cidr_subnet_loadbalancer_b  = var.cidr_loadbalancer_b
+  cidr_subnet_loadbalancer_a  = var.cidr_subnet_loadbalancer_a
+  cidr_subnet_loadbalancer_b  = var.cidr_subnet_loadbalancer_b
   cidr_subnet_apps_a = var.cidr_subnet_apps_a
   cidr_subnet_apps_b = var.cidr_subnet_apps_b
   ssh_allowed_cidr      = var.ssh_allowed_cidr
